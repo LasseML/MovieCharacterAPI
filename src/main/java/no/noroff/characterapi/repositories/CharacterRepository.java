8675304d1e04e95b39916f902956b4e92c1f79a0
kagespiser <kagespiser@outlook.com>
2021-02-10 12:48:28 +0100

    feat: Split application properties into local and production

--- src/main/java/no/noroff/characterapi/repositories/CharacterRepository.java
+++ src/main/java/no/noroff/characterapi/repositories/CharacterRepository.java
@@ -1,6 +1,7 @@
 package no.noroff.characterapi.repositories;
 
+import no.noroff.characterapi.models.MovieCharacter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CharacterRepository extends JpaRepository<Character, Long> {
+public interface CharacterRepository extends JpaRepository<MovieCharacter, Long> {
 }

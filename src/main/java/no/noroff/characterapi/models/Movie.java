9d40ef95d98c401c979e7c7e6c22ecc2dc7919d9
satu.heikkonen <satu.heikkonen@5CG05206R0.nordic.local>
2021-02-10 13:59:14 +0200

    feat: movie model v1

--- src/main/java/no/noroff/characterapi/models/Movie.java
+++ src/main/java/no/noroff/characterapi/models/Movie.java
@@ -1,15 +1,80 @@
 package no.noroff.characterapi.models;
 
+import javax.persistence.*;
 import java.util.Date;
-import java.util.List;
 
+@Entity
 public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name = "title")
     private String title;
+    @Column(name = "genre")
     private String genre;
+    @Column(name = "release")
     private Date release;
+    @Column(name = "director")
     private String director;
+    @Column(name = "picture")
     private String picture;
+    @Column(name = "trailer")
     private String trailer;
-    List<MovieCharacter> movieCharacters;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public Date getRelease() {
+        return release;
+    }
+
+    public void setRelease(Date release) {
+        this.release = release;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getTrailer() {
+        return trailer;
+    }
+
+    public void setTrailer(String trailer) {
+        this.trailer = trailer;
+    }
 }

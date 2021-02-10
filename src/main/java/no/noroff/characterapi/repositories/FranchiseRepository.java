05416b52ffe4e41cc1d8a2e630eb5a194139cb19
kagespiser <kagespiser@outlook.com>
2021-02-10 15:35:33 +0100

    feat: added The @Repository to repositories

--- src/main/java/no/noroff/characterapi/repositories/FranchiseRepository.java
+++ src/main/java/no/noroff/characterapi/repositories/FranchiseRepository.java
@@ -2,6 +2,8 @@ package no.noroff.characterapi.repositories;
 
 import no.noroff.characterapi.models.Franchise;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FranchiseRepository extends JpaRepository<Franchise, Long> {
 }

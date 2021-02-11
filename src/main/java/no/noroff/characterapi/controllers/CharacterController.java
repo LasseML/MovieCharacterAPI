65bf98460aa9b6a1d1129c9a9eaaee3fe2dc7acc
kagespiser <kagespiser@outlook.com>
2021-02-11 14:03:16 +0100

    feat: added simple movieCharacter controller

--- src/main/java/no/noroff/characterapi/controllers/CharacterController.java
+++
@@ -1,9 +0,0 @@
-package no.noroff.characterapi.controllers;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RequestMapping(ApiConstants.CHARACTER_PATH)
-@RestController
-public class CharacterController {
-}
